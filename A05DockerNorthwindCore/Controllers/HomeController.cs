f765196e85099aa5176531c84eb1ebdd50291256
SH <steffen.hardacker@gmx.de>
2018-09-18 14:25:31 +0200

    Queries work, Ajax doesn't

--- A05DockerNorthwindCore/Controllers/HomeController.cs
+++ A05DockerNorthwindCore/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using A05DockerNorthwindCore.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace A05DockerNorthwindCore.Controllers
 {
@@ -26,15 +27,52 @@ namespace A05DockerNorthwindCore.Controllers
 
         public IActionResult FilterCustomers(string country)
         {
-            //TODO: hier geht's weiter. Loading-strategie anpassen
-            var customers = ctx.Customers.Where(cust => cust.Country == country);
+            var customers = ctx.Customers
+                .Where(cust => cust.Country == country)
+                .ToList();
             return PartialView("_customers", customers);
         }
 
+        public IActionResult GetOrders(string customerId)
+        {
+            var orders = ctx.Orders
+                .Where(o => o.CustomerId == customerId)
+                .OrderBy(o => o.OrderDate)
+                .Select(o => o.OrderId)
+                .ToList();
+            ViewData["orderId"] = new SelectList(orders);
+            return PartialView("_orders");
+        }
+
+        public IActionResult GetOrderDetails(int orderId)
+        {
+            Orders order = ctx.Orders
+                .Where(o => o.OrderId == orderId)
+                .Include(o => o.OrderDetails)
+                .Include(o => o.Employee)
+                .FirstOrDefault();
+
+            //string bestellt = order.OrderDate.HasValue 
+            //    ? order.OrderDate.Value.ToShortDateString() 
+            //    : "-";
+            //string shipped = order.ShippedDate.HasValue
+            //    ? order.ShippedDate.Value.ToShortDateString()
+            //    : "-";
+
+            //OrderDetails details = order.OrderDetails.First();
+            //string totalPrice = (details.UnitPrice * details.Quantity * (decimal)details.Discount).ToString("C2");
+
+            //string employee = order.Employee.FirstName + " " + order.Employee.LastName;
+
+            return PartialView("_orderDetails", order);
+        }
+
+        #region Error
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #endregion
     }
 }
---
+++ A05DockerNorthwindCore/Helpers/HtmlHelpers.cs
@@ -0,0 +1,97 @@
+﻿using A05DockerNorthwindCore.Models;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A05DockerNorthwindCore.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static IHtmlContent DisplayOrder(this IHtmlHelper helper, Orders order)
+        {
+            string header =
+                "<thead><tr>" +
+                "<th>BestellNr</th>" +
+                "<th>Bestellt</th>" +
+                "<th>Ausgeliefert</th>" +
+                "<th>Gesamtwert</th>" +
+                "<th>Bearbeitet durch</th>" +
+                "</tr></thead>";
+
+            string ordered = order.OrderDate.HasValue ? order.OrderDate.Value.ToShortDateString() : "-";
+            string shipped = order.ShippedDate.HasValue ? order.ShippedDate.Value.ToShortDateString() : "-";
+            string employee = order.Employee.FirstName + " " + order.Employee.LastName;
+
+            decimal sum = 0;
+            foreach (OrderDetails detail in order.OrderDetails)
+            {
+                decimal price = detail.UnitPrice * detail.Quantity;
+                price += price * (decimal)detail.Discount;
+                sum += price;
+            }
+            string totalPrice = sum.ToString("C2");
+
+            string body =
+                "<tbody><tr>" +
+                $"<td>{order.OrderId}</td>" +
+                $"<td>{ordered}</td>" +
+                $"<td>{shipped}</td>" +
+                $"<td>{totalPrice}</td>" +
+                $"<td>{employee}</td>" +
+                "</tr></tbody>";
+
+            return new HtmlString(
+                $"<table class=\"table\">{header}{body}</table>");
+        }
+
+        public static IHtmlContent DisplayOrder2(this IHtmlHelper helper, Orders order)
+        {
+
+
+            string ordered = order.OrderDate.HasValue ? order.OrderDate.Value.ToShortDateString() : "-";
+            string shipped = order.ShippedDate.HasValue ? order.ShippedDate.Value.ToShortDateString() : "-";
+            string employee = order.Employee.FirstName + " " + order.Employee.LastName;
+
+            decimal totalPrice = 0;
+            foreach (OrderDetails detail in order.OrderDetails)
+            {
+                decimal discount = 1.0m + (decimal)detail.Discount;
+                totalPrice += detail.UnitPrice * discount * detail.Quantity;
+            }
+            string totalPriceStr = totalPrice.ToString("C2");
+
+            string table =
+                 "<table class=\"table\">" +
+                     "<thead>" +
+                         "<tr>" +
+                             "<th>BestellNr</th>" +
+                            $"<th>{order.OrderId}</th>" +
+                         "</tr>" +
+                     "</thead>" +
+                     "<tbody>" +
+                         "<tr>" +
+                             "<td>Bestellt</td>" +
+                            $"<td>{ordered}</td>" +
+                         "</tr>" +
+                         "<tr>" +
+                             "<td>Ausgeliefert</td>" +
+                            $"<td>{shipped}</td>" +
+                         "</tr>" +
+                         "<tr>" +
+                             "<td>Gesamtwert</td>" +
+                            $"<td>{totalPriceStr}</td>" +
+                         "</tr>" +
+                         "<tr>" +
+                             "<td>Bearbeitet durch</td>" +
+                            $"<td>{employee}</td>" +
+                         "</tr>" +
+                     "</tbody>" +
+                 "</table>";
+
+            return new HtmlString(table);
+        }
+    }
+}
